c471bb0e7846aed88119f47dfd556ac85a4450b3
“Begzod <begzod5191305@gmail.com>
2022-10-30 13:42:28 +0500

    initial commit

---
+++ 00010358/Controllers/StudentController.cs
@@ -0,0 +1,155 @@
+﻿using _00010358.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00010358.Controllers
+{
+    public class StudentController : Controller
+    {
+        Uri baseAdd = new Uri("https://localhost:44303/");
+        HttpClient clnt;
+
+        public StudentController()
+        {
+            clnt = new HttpClient();
+            clnt.BaseAddress = baseAdd;
+        }
+
+        // GET: StudentController
+        public async Task<ActionResult> Index()
+        {
+            //Hosted web API REST Service base url
+            string Baseurl = "http://ec2-18-183-196-155.ap-northeast-1.compute.amazonaws.com/";
+            List<Student> StuInfo = new List<Student>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new
+                MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/Student");
+
+                //Checking the response is successful or not which is sent HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var StResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing the Product list
+                    StuInfo = JsonConvert.DeserializeObject<List<Student>>(StResponse);
+                }
+                //returning the Product list to view
+                return View(StuInfo);
+            }
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            Student modelStu = new Student();
+            HttpResponseMessage resStudent = clnt.GetAsync(clnt.BaseAddress + "api/Student/" + id).Result;
+            if (resStudent.IsSuccessStatusCode)
+            {
+                string info = resStudent.Content.ReadAsStringAsync().Result;
+                modelStu = JsonConvert.DeserializeObject<Student>(info);
+            }
+
+            return View(modelStu);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student stu)
+        {
+            string info = JsonConvert.SerializeObject(stu);
+            StringContent cont = new StringContent(info, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage resStudent = clnt.PostAsync(clnt.BaseAddress + "api/Student", cont).Result;
+            if (resStudent.IsSuccessStatusCode)
+                return RedirectToAction(nameof(Index));
+
+            return View();
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Student modelStu = new Student();
+            HttpResponseMessage resStudent = clnt.GetAsync(clnt.BaseAddress + "api/Student/" + id).Result;
+            if (resStudent.IsSuccessStatusCode)
+            {
+                string info = resStudent.Content.ReadAsStringAsync().Result;
+                modelStu = JsonConvert.DeserializeObject<Student>(info);
+            }
+
+            return View(modelStu);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Student stu)
+        {
+            string info = JsonConvert.SerializeObject(stu);
+            StringContent cont = new StringContent(info, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage resStudent = clnt.PutAsync(clnt.BaseAddress + "api/Student/" + stu.StudentID, cont).Result;
+            if (resStudent.IsSuccessStatusCode)
+                return RedirectToAction(nameof(Index));
+            return View();
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Student modelStu = new Student();
+            HttpResponseMessage resStudent = clnt.GetAsync(clnt.BaseAddress + "api/Student/" + id).Result;
+            if (resStudent.IsSuccessStatusCode)
+            {
+                string info = resStudent.Content.ReadAsStringAsync().Result;
+                modelStu = JsonConvert.DeserializeObject<Student>(info);
+            }
+
+            return View(modelStu);
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Student stu)
+        {
+            try
+            {
+                HttpResponseMessage resStudent = clnt.DeleteAsync(clnt.BaseAddress + "api/Student/" + id).Result;
+
+                if (resStudent.IsSuccessStatusCode)
+                    return RedirectToAction(nameof(Index));
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 00010358/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _00010358.Models
+{
+    public class Student
+    {
+        [DisplayName("ID")]
+        public int StudentID { get; set; }
+
+        [DisplayName("First Name")]
+        [Required(ErrorMessage = "Enter your first name here!")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        [Required(ErrorMessage = "Enter your last name here!")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Enter your gmail address here!")]
+        [RegularExpression(@"\w+([-+.]\w+)*@(gmail\.com)", ErrorMessage = "Enter your gmail address here!")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Enter your age here!")]
+        public int Age { get; set; }
+
+        [Phone]
+        [Required(ErrorMessage = "Enter your phone here!")]
+        [RegularExpression(@"^[+]{1}[9]{1}[9]{1}[8]{1}[9]{1}[0-9]{8}$", ErrorMessage = "Enter your phone number starting with +9989...!")]
+        public string Phone { get; set; }
+
+        [DisplayName("Course ID")]
+        [Required(ErrorMessage = "Please select one of the options")]
+        public int CourseId { get; set; }
+    }
+}
